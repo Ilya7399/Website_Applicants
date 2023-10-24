edea22822178153078dc6932369fefa6f3fa4018
unknown <ilyusha.kashira@gmail.com>
2023-10-24 11:42:34 +0300

    answers and questions animation

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
-const second = document.getElementById('mssecond');
-const third = document.getElementById('msthird');
-const fourth = document.getElementById('msfourth');
-const fifth = document.getElementById('msfifth');
+const second = document.getElementById('mssecond')
+const third = document.getElementById('msthird')
+const fourth = document.getElementById('msfourth')
+const fifth = document.getElementById('msfifth')
+const question = document.getElementById('question')
+const answer = document.getElementById('answer')
 
 const elems = document.querySelectorAll('.direction-of-preparation__article').forEach(
     article => {
       article.onmouseenter = function (e) {
-        var psId = article.id.replace('d', 'ps');
-        document.getElementById(psId).style.display = 'flex';
+        var psId = article.id.replace('d', 'ps')
+        document.getElementById(psId).style.display = 'flex'
         document.getElementById(psId).style.animation = 'appearance 1s ease-in-out'
         if (article.id != 'dthird') {
           document.getElementById('msfirst').style.display = 'none'
-          second.style.display =mptitle.style.display= third.style.display= fourth.style.display= fifth.style.display = 'flex';
-          second.style.animation =mptitle.style.animation= third.style.animation= fourth.style.animation= fifth.style.animation = 'appearance 1s ease-in-out';
+          second.style.display =mptitle.style.display= third.style.display= fourth.style.display= fifth.style.display = 'flex'
+          second.style.animation =mptitle.style.animation= third.style.animation= fourth.style.animation= fifth.style.animation = 'appearance 1s ease-in-out'
         }
          else {
           second.style.display = third.style.display= fifth.style.display = 'none';
-          document.getElementById('msfirst').style.display =mptitle.style.display= fourth.style.display= fifth.style.display = 'flex';
-          document.getElementById('msfirst').style.animation =mptitle.style.animation= fourth.style.animation= fifth.style.animation = 'appearance 1s ease-in-out';
+          document.getElementById('msfirst').style.display =mptitle.style.display= fourth.style.display= fifth.style.display = 'flex'
+          document.getElementById('msfirst').style.animation =mptitle.style.animation= fourth.style.animation= fifth.style.animation = 'appearance 1s ease-in-out'
         }
       }
       article.onmouseleave = function (e){
-        var psId = article.id.replace('d', 'ps');
+        var psId = article.id.replace('d', 'ps')
         document.getElementById(psId).style.animation = 'disappearance 1s ease-in-out'
-        setTimeout(function() {document.getElementById(psId).style.display = 'none'}, 900);
+        setTimeout(function() {document.getElementById(psId).style.display = 'none'}, 900)
       }
     }
  )
@@ -38,8 +40,19 @@ const observer = new IntersectionObserver((entries)=>{
     }
   })
 })
- const hiddenElements = document.querySelectorAll('.admission-stage');
+ const hiddenElements = document.querySelectorAll('.admission-stage')
  hiddenElements.forEach((el)=>observer.observe(el))
+
+ const questions = document.querySelectorAll('.questions-answers__article').forEach(
+  article => {
+    article.onmouseenter = function (e) {
+      var psId = article.id.replace('q', 'a')
+      document.getElementById(psId).style.display = 'flex'
+      document.getElementById(psId).style.animation = 'appearance 1s ease-in-out'
+    }
+  }
+)
+
 
 
 
